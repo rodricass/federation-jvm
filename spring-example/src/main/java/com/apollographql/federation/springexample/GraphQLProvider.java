da542918ab22633c9037848785240c0ac04e5e4a
Rodrigo <rodrigo.ncassanelli@gmail.com>
2020-10-16 15:22:21 -0300

    Federation example

---
+++ spring-example/src/main/java/com/apollographql/federation/springexample/GraphQLProvider.java
@@ -0,0 +1,39 @@
+package com.apollographql.federation.springexample;
+
+import com.apollographql.federation.graphqljava.Federation;
+import com.coxautodev.graphql.tools.SchemaParser;
+import graphql.schema.DataFetcher;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.StaticDataFetcher;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.servlet.config.DefaultGraphQLSchemaProvider;
+import graphql.servlet.config.GraphQLSchemaProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+import static graphql.schema.idl.TypeRuntimeWiring.newTypeWiring;
+
+import javax.management.Query;
+import java.io.IOException;
+
+@Component
+public class GraphQLProvider extends DefaultGraphQLSchemaProvider implements GraphQLSchemaProvider {
+    @Autowired
+    private static GraphQLDataFetchers graphQLDataFetchers;
+
+    public GraphQLProvider(@Value("classpath:schemas/inventory.graphqls") Resource sdl) throws IOException {
+        super(Federation.transform(sdl.getFile(), GraphQLProvider.buildWiring())
+                .build());
+    }
+
+    private static RuntimeWiring buildWiring() {
+        return RuntimeWiring.newRuntimeWiring()
+                .type(newTypeWiring("Query")
+                        .dataFetcher("bookById", graphQLDataFetchers.getBookByIdDataFetcher()))
+                .type(newTypeWiring("Book")
+                        .dataFetcher("author", graphQLDataFetchers.getAuthorDataFetcher()))
+                .build();
+    }
+}
